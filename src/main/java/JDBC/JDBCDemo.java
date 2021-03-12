8c410d8868e37cbe3e5d1503414606fda094b894
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-12 10:33:04 -0600

    added config file example and readme safer call

--- src/main/java/JDBC/JDBCDemo.java
+++ src/main/java/JDBC/JDBCDemo.java
@@ -1,4 +1,54 @@
 package JDBC;
+import java.sql.*;
+import java.util.ArrayList;
+import com.mysql.cj.jdbc.Driver;
 
 public class JDBCDemo {
+
+    private static ArrayList<Album> albums = new ArrayList<>();
+    static Config config = new Config();
+
+    public static void main(String[] args) {
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            Connection connection = DriverManager.getConnection (
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM albums");
+
+            //Transforming the ResultSet into a Java Collection
+            while (rs.next()) {
+                albums.add(new Album (
+                        rs.getLong("id"),
+                        rs.getString("artist"),
+                        rs.getString("name"),
+                        rs.getInt("release_date"),
+                        rs.getFloat("sales"),
+                        rs.getString("genre")
+                ));
+            }
+            printAlbums(albums);
+
+//            statement.executeUpdate("INSERT INTO albums (artist, name, release_date, genre, sales) VALUES('Nelly Furtado', 'Loose', 2006, 'Dance-pop, hip hop, R&B', 12.5)");
+            rs = statement.executeQuery("SELECT * FROM albums");
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    public static void printAlbums(ArrayList<Album> albums) {
+        for (Album album: albums) {
+            System.out.println(album.getArtist());
+        }
+    }
+
+
 }
