5c2928f982f4c3742041fa0b9ae3d139ec86731f
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-13 18:49:26 -0600

    adding getters & setters for author

--- src/main/java/Javabeans/Author.java
+++ src/main/java/Javabeans/Author.java
@@ -15,6 +15,25 @@ public class Author {
         this.lastName = lastName;
     }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
