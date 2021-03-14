0be2774a38ecf962828ba934cd27a85fae6e8193
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-13 18:48:51 -0600

    adding author javabean & constructor

---
+++ src/main/java/Javabeans/Author.java
@@ -0,0 +1,20 @@
+package Javabeans;
+
+public class Author {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    // Javabean
+    public Author() {};
+
+    public Author(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+}
