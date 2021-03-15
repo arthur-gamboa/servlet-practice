3b577f59b66156370b9f7a8a246cfa054fadd966
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-14 22:34:40 -0500

    created DaoFactory class

---
+++ src/main/java/DAOs/DaoFactory.java
@@ -0,0 +1,13 @@
+package DAOs;
+
+public class DaoFactory {
+
+    private static Ads adsDao;
+    public static Ads getAdsDao() {
+        if (adsDao == null) {
+            adsDao = new MySQLAdsDao();
+        }
+        return adsDao;
+
+    }
+}
